393465f0150ac0da7e0af6cb48d787a5eda5c551
Papymankan <parsa84rostami@gmail.com>
2024-09-01 19:13:31 +0330

    fix home

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Card({to , title}) {
+export default function Card({ to, title }) {
   return (
     <Link to={to} className="w-full sm:w-1/2 md:w-[31%]">
-      <div className="group mt-6 cursor-pointer w-full rounded-md bg-white p-4 shadow-2xl hover:scale-105 transition-all">
-        <img src="/img/test.png" className="w-90 mx-auto rounded-md group-hover:w-full bg-cover bg-center" />
+      <div className="group mt-6 w-full cursor-pointer rounded-md bg-slate-300 p-4 shadow-2xl transition-all hover:scale-105">
+        <img
+          src="/img/test.png"
+          className="mx-auto aspect-video w-full rounded-md bg-cover bg-center object-cover"
+        />
         <h3 className="mt-6 text-xl text-slate-900">{title}</h3>
       </div>
     </Link>
