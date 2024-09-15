d1b7d93b5045683636d2a6de97a4b7eb714fe3ce
Papymankan <parsa84rostami@gmail.com>
2024-09-15 09:41:13 +0330

    other boxes testimonial

--- src/Pages/TestimonailGrid/TestimonailGrid.jsx
+++ src/Pages/TestimonailGrid/TestimonailGrid.jsx
@@ -3,9 +3,10 @@ import React from "react";
 export default function TestimonailGrid() {
   return (
     <>
-      <div className="font-grid min-h-screen w-full bg-gray-200">
+      <div className="min-h-screen w-full bg-gray-200 font-grid">
         <div className="container mx-auto max-w-7xl border-2 border-black p-2 md:p-10">
           <div className="text-white">
+            {/* Box 1 */}
             <div className="relative rounded-xl bg-purple-700 p-10">
               <img
                 src="/img/assets/Testimonial-grid/bg-pattern-quotation.svg"
@@ -15,11 +16,13 @@ export default function TestimonailGrid() {
               <div className="z-20 flex items-center">
                 <img
                   src="/img/assets/Testimonial-grid/image-daniel.jpg"
-                  className="rounded-full border-2"
+                  className="rounded-full border-2 border-purple-300"
                   alt=""
                 />
                 <div className="ml-5">
-                  <h3 className="text-lg font-bold opacity-90">Daniel Clifford</h3>
+                  <h3 className="text-lg font-bold opacity-90">
+                    Daniel Clifford
+                  </h3>
                   <p className="text-gray-200">Verified Graduate</p>
                 </div>
               </div>
@@ -43,6 +46,156 @@ export default function TestimonailGrid() {
                 omnis deserunt? Sit nam molestiae excepturi, perspiciatis labore
                 deleniti eum, eligendi quisquam quod sed nobis exercitationem
                 tempora! "
+              </p>
+            </div>
+
+            {/* Box 2 */}
+            <div className="relative rounded-xl bg-gray-600 p-10">
+              <div className="flex items-center">
+                <img
+                  src="/img/assets/Testimonial-grid/image-jonathan.jpg"
+                  className="rounded-full border-2 border-purple-300"
+                  alt=""
+                />
+                <div className="ml-5">
+                  <h3 className="text-lg font-bold opacity-90">
+                    Jonathan Walters
+                  </h3>
+                  <p className="text-gray-200">Verified Graduate</p>
+                </div>
+              </div>
+              <div className="relative mt-10 text-xl font-bold">
+                The team was very supportive and kept me motivated
+              </div>
+              <p class="mt-6 line-clamp-6 opacity-70">
+                "I started as a total newbie with virtually no coding skills. I
+                now work as a mobile engineer for a big company. This was one of
+                the best investments I've made in myself. Lorem ipsum dolor sit
+                amet consectetur adipisicing elit. Atque ipsam veritatis natus
+                illum ullam ab, nesciunt rerum nobis, quibusdam ad voluptatum
+                rem necessitatibus facere nihil deleniti assumenda vel velit
+                neque quia, nisi ipsum laudantium impedit eveniet iste.
+                Pariatur, exercitationem consequuntur praesentium a est eum
+                velit quos maiores ipsum facere tempora!"
+              </p>
+            </div>
+
+            {/* Box 3 */}
+            <div className="hidden relative rounded-xl bg-white p-10 text-gray-900">
+              <div className="flex items-center">
+                <img
+                  src="/img/assets/Testimonial-grid/image-kira.jpg"
+                  className="rounded-full border-2 border-purple-300"
+                  alt=""
+                />
+                <div className="ml-5">
+                  <h3 className="text-lg font-bold opacity-90">Kira Whittle</h3>
+                  <p className="text-gray-200">Verified Graduate</p>
+                </div>
+              </div>
+              <div className="relative mt-10 text-xl font-bold">
+                Such a life-changing experience. Highly recommended!
+              </div>
+              <p class="mt-6 line-clamp-6 opacity-70">
+                "Before joining the bootcamp, I've never written a line of code.
+                I needed some structure from professionals who can help me learn
+                programming step by step. I was encouraged to enroll by a former
+                student of theirs who can only say wonderful things about the
+                program. The entire curriculum and staff did not disappoint.
+                They were very hands-on and I never had to wait long for
+                assistance. The agile team project, in particular, was
+                outstanding. It took my learning to the next level in a way that
+                no tutorial could ever have. In fact, I've often referred to it
+                during interviews as an example of my developent experience. It
+                certainly helped me land a job as a full-stack developer after
+                receiving multiple offers. 100% recommend!"
+              </p>
+            </div>
+
+            {/* Box 4 */}
+            <div className="relative rounded-xl bg-white p-10 text-gray-900">
+              <div className="flex items-center">
+                <img
+                  src="/img/assets/Testimonial-grid/image-jeanette.jpg"
+                  className="rounded-full border-2 border-purple-300"
+                  alt=""
+                />
+                <div className="ml-5">
+                  <h3 className="text-lg font-bold opacity-90">
+                    Jeanette Harmon
+                  </h3>
+                  <p className="text-gray-200">Verified Graduate</p>
+                </div>
+              </div>
+              <div className="relative mt-10 text-xl font-bold">
+                An overall wonderful and rewarding experience
+              </div>
+              <p class="mt-6 line-clamp-6 opacity-70">
+                "Thank you for the wonderful experience! I now have a job I
+                really enjoy, and make a good living while doing something I
+                love."
+              </p>
+            </div>
+
+            {/* Box 5 */}
+            <div className="relative rounded-xl bg-gray-900 p-10">
+              <div className="flex items-center">
+                <img
+                  src="/img/assets/Testimonial-grid/image-patrick.jpg"
+                  className="rounded-full border-2 border-purple-300"
+                  alt=""
+                />
+                <div className="ml-5">
+                  <h3 className="text-lg font-bold opacity-90">
+                    Patrick Abrams
+                  </h3>
+                  <p className="text-gray-200">Verified Graduate</p>
+                </div>
+              </div>
+              <div className="relative mt-10 text-xl font-bold">
+                Awesome teaching support from TAs who did the bootcamp
+                themselves. Getting guidance from them and learning from their
+                experiences was easy.
+              </div>
+              <p class="mt-6 line-clamp-6 opacity-70">
+                "The staff seem genuinely concerned about my progress which I
+                find really refreshing. The program gave me the confidence
+                necessary to be able to go out in the world and present myself
+                as a capable junior developer. The standard is above the rest.
+                You will get the personal attention you need from an incredible
+                community of smart and amazing people."
+              </p>
+            </div>
+
+            {/* Box 6 */}
+            <div className="relative rounded-xl bg-white p-10 text-gray-900">
+              <div className="flex items-center">
+                <img
+                  src="/img/assets/Testimonial-grid/image-kira.jpg"
+                  className="rounded-full border-2 border-purple-300"
+                  alt=""
+                />
+                <div className="ml-5">
+                  <h3 className="text-lg font-bold opacity-90">Kira Whittle</h3>
+                  <p className="text-gray-200">Verified Graduate</p>
+                </div>
+              </div>
+              <div className="relative mt-10 text-xl font-bold">
+                Such a life-changing experience. Highly recommended!
+              </div>
+              <p class="mt-6 line-clamp-6 opacity-70">
+                "Before joining the bootcamp, I've never written a line of code.
+                I needed some structure from professionals who can help me learn
+                programming step by step. I was encouraged to enroll by a former
+                student of theirs who can only say wonderful things about the
+                program. The entire curriculum and staff did not disappoint.
+                They were very hands-on and I never had to wait long for
+                assistance. The agile team project, in particular, was
+                outstanding. It took my learning to the next level in a way that
+                no tutorial could ever have. In fact, I've often referred to it
+                during interviews as an example of my developent experience. It
+                certainly helped me land a job as a full-stack developer after
+                receiving multiple offers. 100% recommend!"
               </p>
             </div>
           </div>
