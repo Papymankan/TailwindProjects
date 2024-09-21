76887d3f219000055e822e44d8fd28f8b98aeb35
Papymankan <parsa84rostami@gmail.com>
2024-09-21 18:55:37 +0330

    footer & UpToDate Section Bookmark

--- src/Pages/Bookmark/Bookmark.jsx
+++ src/Pages/Bookmark/Bookmark.jsx
@@ -6,7 +6,7 @@ export default function Bookmark() {
   const [accordion, setAccordion] = useState(2);
 
   return (
-    <div className="min-h-screen w-full overflow-hidden bg-white font-rubik">
+    <div className="min-h-screen w-full overflow-hidden scroll-smooth bg-white font-rubik">
       {/* Header */}
       <div className="container sticky mx-auto flex items-center justify-between px-4 py-8">
         <svg
@@ -32,13 +32,13 @@ export default function Bookmark() {
         </svg>
 
         <div className="hidden items-center space-x-10 md:flex">
-          <a href="#" className="text-lg uppercase text-grayishBlue">
+          <a href="#features" className="text-lg uppercase text-grayishBlue">
             features
           </a>
-          <a href="#" className="text-lg uppercase text-grayishBlue">
+          <a href="#download" className="text-lg uppercase text-grayishBlue">
             download
           </a>
-          <a href="#" className="text-lg uppercase text-grayishBlue">
+          <a href="#FAQ" className="text-lg uppercase text-grayishBlue">
             FAQ
           </a>
           <a
@@ -108,7 +108,7 @@ export default function Bookmark() {
       </div>
 
       {/* Features */}
-      <div className="container mx-auto mt-16 px-4">
+      <div className="container mx-auto mt-16 px-4" id="features">
         <h2 className="text-center text-4xl font-semibold">Features</h2>
         <p className="mx-auto mt-5 max-w-sm text-center text-grayishBlue">
           Our aim is to make it quick and easy for you to access your favourite
@@ -127,7 +127,7 @@ export default function Bookmark() {
               onClick={() => setTab(1)}
             >
               <span
-                className={`relative top-0.5 ${tab == 1 && "border-b-4 border-softRed"} py-4 duration-200`}
+                className={`relative top-0.5 ${tab == 1 && "border-b-4 border-softRed"} py-4`}
               >
                 Simple Bookmarking
               </span>
@@ -138,7 +138,7 @@ export default function Bookmark() {
               onClick={() => setTab(2)}
             >
               <span
-                className={`relative top-0.5 ${tab == 2 && "border-b-4 border-softRed"} py-4 duration-200`}
+                className={`relative top-0.5 ${tab == 2 && "border-b-4 border-softRed"} py-4`}
               >
                 Speedy Searching
               </span>
@@ -149,7 +149,7 @@ export default function Bookmark() {
               onClick={() => setTab(3)}
             >
               <span
-                className={`relative top-0.5 ${tab == 3 && "border-b-4 border-softRed"} py-4 duration-200`}
+                className={`relative top-0.5 ${tab == 3 && "border-b-4 border-softRed"} py-4`}
               >
                 Easy Sharing
               </span>
@@ -252,7 +252,7 @@ export default function Bookmark() {
       </div>
 
       {/* Download boxes */}
-      <div className="container mx-auto mt-16 px-4">
+      <div className="container mx-auto mt-16 px-4" id="download">
         <h2 className="text-center text-4xl font-semibold">
           Download the extension
         </h2>
@@ -334,7 +334,7 @@ export default function Bookmark() {
       </div>
 
       {/* FAQ */}
-      <div className="container mx-auto mt-40 px-4">
+      <div className={`container mx-auto mt-40 px-4`} id="FAQ">
         <h2 className="text-center text-4xl font-semibold">
           Frequently Asked Questions
         </h2>
@@ -343,10 +343,10 @@ export default function Bookmark() {
           answered please feel free to email us.
         </p>
 
-        <div className="mx-auto mt-10 max-w-2xl overflow-hidden">
+        <div className="mx-auto mt-10 h-[500px] max-w-2xl overflow-hidden md:h-[450px] lg:h-[405px]">
           <div className="mb-4 w-full border-b">
             <div
-              className="group veryDarkBlue2 flex w-full cursor-pointer items-center justify-between py-4 text-lg"
+              className="veryDarkBlue2 group flex w-full cursor-pointer items-center justify-between py-4 text-lg"
               onClick={() => {
                 if (accordion == 2) {
                   setAccordion(0);
@@ -355,7 +355,9 @@ export default function Bookmark() {
                 }
               }}
             >
-              <p  className="group-hover:text-softRed duration-200">What is Bookmark ?</p>
+              <p className="duration-200 group-hover:text-softRed">
+                What is Bookmark ?
+              </p>
               <button>
                 <img
                   src="/img/assets/Bookmark/icon-arrow.svg"
@@ -365,7 +367,7 @@ export default function Bookmark() {
               </button>
             </div>
             <div
-              className={`w-full overflow-hidden text-justify text-grayishBlue ${accordion != 2 ? "max-h-0" : "max-h-screen"} transition-all duration-300 ease-in-out mb-2`}
+              className={`w-full overflow-hidden text-justify text-grayishBlue ${accordion != 2 ? "max-h-0" : "max-h-screen"} mb-2 transition-all duration-300 ease-in-out`}
             >
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
               repellat amet doloribus consequuntur eos similique provident
@@ -376,7 +378,7 @@ export default function Bookmark() {
 
           <div className="mb-4 w-full border-b">
             <div
-              className="group veryDarkBlue2 flex w-full cursor-pointer items-center justify-between py-4 text-lg"
+              className="veryDarkBlue2 group flex w-full cursor-pointer items-center justify-between py-4 text-lg"
               onClick={() => {
                 if (accordion == 1) {
                   setAccordion(0);
@@ -385,7 +387,9 @@ export default function Bookmark() {
                 }
               }}
             >
-              <p className="group-hover:text-softRed duration-200">How can I request a new browser ?</p>
+              <p className="duration-200 group-hover:text-softRed">
+                How can I request a new browser ?
+              </p>
               <button>
                 <img
                   src="/img/assets/Bookmark/icon-arrow.svg"
@@ -395,7 +399,7 @@ export default function Bookmark() {
               </button>
             </div>
             <div
-              className={`w-full overflow-hidden text-justify text-grayishBlue ${accordion != 1 ? "max-h-0" : "max-h-screen"} transition-all duration-300  ease-in-out  mb-2`}
+              className={`w-full overflow-hidden text-justify text-grayishBlue ${accordion != 1 ? "max-h-0" : "max-h-screen"} mb-2 transition-all duration-300 ease-in-out`}
             >
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
               repellat amet doloribus consequuntur eos similique provident
@@ -406,7 +410,7 @@ export default function Bookmark() {
 
           <div className="mb-4 w-full border-b">
             <div
-              className="group veryDarkBlue2 flex w-full cursor-pointer items-center justify-between py-4 text-lg"
+              className="veryDarkBlue2 group flex w-full cursor-pointer items-center justify-between py-4 text-lg"
               onClick={() => {
                 if (accordion == 3) {
                   setAccordion(0);
@@ -415,7 +419,9 @@ export default function Bookmark() {
                 }
               }}
             >
-              <p className="group-hover:text-softRed duration-200">Is ther a mobile app ?</p>
+              <p className="duration-200 group-hover:text-softRed">
+                Is ther a mobile app ?
+              </p>
               <button>
                 <img
                   src="/img/assets/Bookmark/icon-arrow.svg"
@@ -425,7 +431,7 @@ export default function Bookmark() {
               </button>
             </div>
             <div
-              className={`w-full overflow-hidden text-justify text-grayishBlue ${accordion != 3 ? "max-h-0" : "max-h-screen"} transition-all duration-300  ease-in-out  mb-2`}
+              className={`w-full overflow-hidden text-justify text-grayishBlue ${accordion != 3 ? "max-h-0" : "max-h-screen"} mb-2 transition-all duration-300 ease-in-out`}
             >
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
               repellat amet doloribus consequuntur eos similique provident
@@ -436,7 +442,7 @@ export default function Bookmark() {
 
           <div className="mb-4 w-full">
             <div
-              className="group veryDarkBlue2 flex w-full cursor-pointer items-center justify-between py-4 text-lg"
+              className="veryDarkBlue2 group flex w-full cursor-pointer items-center justify-between py-4 text-lg"
               onClick={() => {
                 if (accordion == 4) {
                   setAccordion(0);
@@ -445,7 +451,9 @@ export default function Bookmark() {
                 }
               }}
             >
-              <p className="group-hover:text-softRed duration-200">What about other Chromium browsers ?</p>
+              <p className="duration-200 group-hover:text-softRed">
+                What about other Chromium browsers ?
+              </p>
               <button>
                 <img
                   src="/img/assets/Bookmark/icon-arrow.svg"
@@ -455,13 +463,74 @@ export default function Bookmark() {
               </button>
             </div>
             <div
-              className={`w-full overflow-hidden text-justify text-grayishBlue ${accordion != 4 ? "max-h-0" : "max-h-screen"} transition-all duration-300  ease-in-out  mb-2`}
+              className={`w-full overflow-hidden text-justify text-grayishBlue ${accordion != 4 ? "max-h-0" : "max-h-screen"} mb-2 transition-all duration-300 ease-in-out`}
             >
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
               repellat amet doloribus consequuntur eos similique provident
               tempora voluptates iure quia fuga dicta voluptatibus culpa
               mollitia recusandae delectus id suscipit labore?
             </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Up to date */}
+
+      <div className="w-full bg-softBlue py-20 text-white">
+        <div className="container mx-auto px-4">
+          <h3 className="text-center text-2xl">35,000+ Already Joined</h3>
+          <h1 className="mx-auto mt-8 max-w-xl text-center text-4xl font-bold">
+            Stay up-to-date with what we're doing
+          </h1>
+          <div className="mt-8 flex w-full flex-col items-center justify-center space-y-5 lg:flex-row lg:space-x-5 lg:space-y-0">
+            <input
+              type="text"
+              className="min-w-72 rounded-md p-2 text-lg text-black"
+              placeholder="Bookmark@example.com"
+            />
+            <a
+              href="#"
+              className="rounded-md bg-softRed px-8 py-2 text-lg uppercase text-white hover:opacity-80 duration-200"
+            >
+              Contact Us
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/* Footer */}
+
+      <div className="w-full bg-veryDarkBlue2 py-10">
+        <div className="flex container mx-auto flex-col items-center justify-between md:flex-row px-4">
+          <div className="flex flex-col items-center space-y-5 md:flex-row md:space-x-10 md:space-y-0 text-grayishBlue">
+            <svg
+              width="148"
+              height="25"
+              xmlns="http://www.w3.org/2000/svg"
+              className="z-20"
+            >
+              <g fill="none" fill-rule="evenodd">
+                <path
+                  d="M37 6.299h5.227c.746 0 1.434.155 2.062.466.629.311 1.123.735 1.484 1.27s.542 1.12.542 1.754c0 .672-.165 1.254-.495 1.746-.33.491-.762.868-1.297 1.129v.15c.697.248 1.25.643 1.661 1.185.41.541.616 1.191.616 1.95 0 .735-.196 1.385-.588 1.951a3.817 3.817 0 0 1-1.587 1.307c-.665.305-1.403.457-2.212.457H37V6.299zm5.04 5.45c.548 0 .986-.152 1.316-.457.33-.305.495-.688.495-1.148 0-.448-.159-.824-.476-1.13-.318-.304-.738-.457-1.26-.457H39.52v3.192h2.52zm.28 5.619c.61 0 1.086-.159 1.428-.476.342-.317.513-.731.513-1.241 0-.51-.174-.927-.522-1.251-.349-.324-.847-.485-1.494-.485H39.52v3.453h2.8zm12.927 2.595c-1.307 0-2.492-.308-3.556-.924a6.711 6.711 0 0 1-2.511-2.53c-.61-1.07-.915-2.246-.915-3.528 0-1.281.305-2.457.915-3.528a6.711 6.711 0 0 1 2.51-2.529C52.756 6.308 53.94 6 55.248 6c1.306 0 2.492.308 3.556.924a6.711 6.711 0 0 1 2.51 2.53c.61 1.07.915 2.246.915 3.527 0 1.282-.305 2.458-.915 3.528a6.711 6.711 0 0 1-2.51 2.53c-1.064.616-2.25.924-3.556.924zm0-2.39a4.52 4.52 0 0 0 2.258-.578 4.177 4.177 0 0 0 1.615-1.624c.392-.697.588-1.494.588-2.39 0-.896-.196-1.692-.588-2.389a4.177 4.177 0 0 0-1.615-1.624 4.52 4.52 0 0 0-2.258-.579 4.47 4.47 0 0 0-2.25.579 4.195 4.195 0 0 0-1.605 1.624c-.392.697-.588 1.493-.588 2.39 0 .895.196 1.692.588 2.389a4.195 4.195 0 0 0 1.605 1.624 4.47 4.47 0 0 0 2.25.578zm15.353 2.39c-1.307 0-2.492-.308-3.556-.924a6.711 6.711 0 0 1-2.51-2.53c-.61-1.07-.915-2.246-.915-3.528 0-1.281.305-2.457.914-3.528a6.711 6.711 0 0 1 2.511-2.529C68.108 6.308 69.294 6 70.6 6c1.307 0 2.492.308 3.556.924a6.711 6.711 0 0 1 2.51 2.53c.61 1.07.915 2.246.915 3.527 0 1.282-.305 2.458-.914 3.528a6.711 6.711 0 0 1-2.511 2.53c-1.064.616-2.25.924-3.556.924zm0-2.39a4.52 4.52 0 0 0 2.259-.578 4.177 4.177 0 0 0 1.614-1.624c.392-.697.588-1.494.588-2.39 0-.896-.196-1.692-.588-2.389a4.177 4.177 0 0 0-1.614-1.624 4.52 4.52 0 0 0-2.259-.579 4.47 4.47 0 0 0-2.25.579 4.195 4.195 0 0 0-1.605 1.624c-.392.697-.588 1.493-.588 2.39 0 .895.196 1.692.588 2.389a4.195 4.195 0 0 0 1.606 1.624 4.47 4.47 0 0 0 2.249.578zM79.83 6.3h2.52v5.73h.15l4.89-5.73h3.043v.149L85.6 11.973l5.338 7.542v.149h-3.08l-3.994-5.693-1.512 1.773v3.92h-2.52V6.299zM93.779 6h3.248l3.546 9.39h.15L104.268 6h3.267v13.365h-2.501v-6.589l.15-2.221h-.15l-3.398 8.81h-1.96l-3.416-8.81h-.149l.15 2.221v6.59h-2.483V6zm20.8 0h2.894l5.021 13.365h-2.781l-1.12-3.192h-5.115l-1.12 3.192h-2.781L114.579 6zm3.193 7.859l-1.176-3.36-.486-1.606h-.149l-.485 1.606-1.195 3.36h3.49zM124.553 6h4.872c.871 0 1.646.18 2.324.541.678.361 1.204.862 1.577 1.503.374.64.56 1.366.56 2.175 0 .858-.27 1.62-.812 2.286a4.617 4.617 0 0 1-2.044 1.447l-.018.13 3.584 5.134v.15h-2.894l-3.453-5.022h-1.176v5.021h-2.52V6zm4.853 6.03c.573 0 1.04-.175 1.4-.523.361-.349.542-.79.542-1.326 0-.51-.172-.945-.514-1.306-.342-.361-.806-.542-1.39-.542h-2.371v3.696h2.333zm7.23-6.03h2.52v5.73h.15l4.89-5.73h3.043v.15l-4.835 5.525 5.34 7.541v.15h-3.08l-3.996-5.694-1.512 1.773v3.92h-2.52V6z"
+                  fill="white"
+                  fill-rule="nonzero"
+                />
+                <g>
+                  <circle fill="#5267DF" cx="12.5" cy="12.5" r="12.5" />
+                  <path
+                    d="M9 9v10l3.54-3.44L16.078 19V9a2 2 0 0 0-2-2H11a2 2 0 0 0-2 2z"
+                    fill="#FFF"
+                  />
+                </g>
+              </g>
+            </svg>
+            <a href="#" className="uppercase hover:text-white duration-200 text-lg md:pl-8">features</a>
+            <a href="#" className="uppercase hover:text-white duration-200 text-lg">Pricing</a>
+            <a href="#" className="uppercase hover:text-white duration-200 text-lg">Contact</a>
+          </div>
+          <div className="flex items-center space-x-6 mt-6 md:mt-0">
+              <img src="public/img/assets/Bookmark/icon-twitter.svg" alt=""  className="cursor-pointer" />
+              <img src="public/img/assets/Bookmark/icon-facebook.svg" alt="" className="cursor-pointer"  />
           </div>
         </div>
       </div>
