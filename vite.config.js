d563b8fad9da01b945214d605525fea85a6c9093
Papymankan <parsa84rostami@gmail.com>
2024-09-22 11:16:25 +0330

    redirect error

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        injectData: {
+          // redirect to index.html on 404 error
+          BASE_URL: '/your-repo-name/'
+        },
+      },
+    }),
+  ],
   base:'/TailwindProjects/'
 })
