38467c860803812418c455cae38b873375ab8565
Papymankan <parsa84rostami@gmail.com>
2024-09-22 12:02:44 +0330

    fix UI

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <>
       <div className="container mx-auto mb-4 flex flex-col items-center p-4">
-        <h2 className="mt-12 text-2xl font-bold sm:text-4xl">
+        <h2 className="mt-12 text-2xl font-bold sm:text-4xl text-white">
           Tailwind Mini-Projects
         </h2>
         <div className="w-7/8 mt-8 grid grid-cols-1 grid-rows-5 gap-3 sm:grid-cols-2 sm:grid-rows-3 md:grid-cols-3 md:grid-rows-2">
@@ -12,56 +12,67 @@ function Home() {
             to={"/bookmark"}
             title="BookMark"
             src="/img/assets/Bookmark/thumbnail.png"
+            mode={"Landing Page"}
           />
           <Card
             to={"/fylo"}
             title="Fylo"
             src="/img/assets/Fylo/thumbnail.png"
+            mode={"Landing Page"}
           />
           <Card
             to={"/testimonial-grid"}
             title="Testimonial Grid"
             src="/img/assets/Testimonial-grid/thumbnail.png"
+            mode={"Landing Page"}
           />
           <Card
             to={"/shortly"}
             title="Shortly"
             src="/img/assets/Shortly/thumbnail.png"
+            mode={"Landing Page"}
           />
           <Card
             to={"/loopstudio"}
             title="Loopstudio"
             src="/img/assets/Loopstudio/thumbnail.png"
+            mode={"Landing Page"}
           />
           <Card
             to={"/clipboard"}
             title="Clipboard"
             src="/img/assets/Clipboard/thumbnail.png"
+            mode={"Landing Page"}
           />
           <Card
             to={"/emailcard"}
             title="Email Card"
             src="/img/email-card-thumbnail.png"
+            mode={"Component"}
           />
           <Card
             to={"/pricingcards"}
             title="Pricing Cards"
             src="/img/pricing-cards-thumbnail.png"
+            mode={"Component"}
           />
           <Card
             to={"/productmodal"}
             title="Product Modal"
             src="/img/product-modal-thumbnail.png"
+            mode={"Component"}
           />
           <Card
             to={"/imagegallery"}
             title="Image Gallery"
             src="/img/image-gallery-thumbnail.png"
+            mode={"Component"}
           />
           <Card
             to={"/loginmodal"}
             title="Login Modal"
             src="/img/login-modal-thumbnail.png"
+            mode={"Component"}
           />
         </div>
       </div>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Card({ to, title , src}) {
+export default function Card({ to, title , src , mode}) {
   return (
     <Link to={to} className="w-full ">
-      <div className="group mt-6 w-full cursor-pointer rounded-md bg-slate-300 p-4 shadow-2xl transition-all hover:scale-105">
+      <div className="group mt-6 w-full cursor-pointer rounded-md bg-slate-300 p-4 shadow-2xl transition-all hover:scale-105 flex flex-col">
         <img
           src={src}
           className="mx-auto aspect-video w-full rounded-md bg-cover bg-center object-cover"
         />
         <h3 className="mt-6 text-xl text-slate-900">{title}</h3>
+        <h4 className={`px-2 mt-2 py-1 rounded-full self-start text-white ${mode == 'Component' ? 'bg-orange-500' : 'bg-softBlue'}`}>{mode}</h4>
       </div>
     </Link>
   ); 
