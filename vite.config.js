e2308de05f97cb40fecb1bec1294f892ec7514a8
Papymankan <parsa84rostami@gmail.com>
2024-09-22 11:17:14 +0330

    redirect error2

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
