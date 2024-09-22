c8f38c455ec20ee0e334a6d75a36f74119514a76
Papymankan <parsa84rostami@gmail.com>
2024-09-22 11:30:07 +0330

    clear

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { createHtmlPlugin } from 'vite-plugin-html';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    createHtmlPlugin({
-      inject: {
-        injectData: {
-          // redirect to index.html on 404 error
-          BASE_URL: '/your-repo-name/'
-        },
-      },
-    }),
-  ],
-  base:'/TailwindProjects/'
-})
+  plugins: [react()],
+});
